40a58088d366ec47f3c25d4e19c490e93dbe6d0c
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 17:42:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+var prefix = "2";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "523495843454779404"; // ايدي السررفر
+var channel = "523495843454779406";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
